b071ba73c82c65a3e7abf8c6cb08381e782304f7
ragnar48h <hasirkhan3175@gmail.com>
2021-12-17 11:52:55 +0530

    setting up next and firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import { CSSReset, ThemeProvider } from '@chakra-ui/core'
+import { Global, css } from '@emotion/core'
+
+import '@/styles/globals.css'
+import {ProvideAuth} from '@/lib/auth'
+import theme from '@/styles/theme'
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <ProvideAuth>
+        <GlobalStyle/>
+        <Component {...pageProps} />
+      </ProvideAuth>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
